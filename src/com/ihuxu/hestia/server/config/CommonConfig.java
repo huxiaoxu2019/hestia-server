776bf577eabe35037b19c2ae6d09029b5c457ad3
GenialX <admin@ihuxu.com>
2017-10-07 00:20:01 +0800

    add identity token

--- src/com/ihuxu/hestia/server/config/CommonConfig.java
+++ src/com/ihuxu/hestia/server/config/CommonConfig.java
@@ -5,4 +5,5 @@ public interface CommonConfig {
     public static final int SERVER_SOCKET_PORT = 1722;
     public static final int SERVER_CRONTAB_CHECK_CLIENT_SOCKET_INTERVAL = 3000;
     public static final int SERVER_CRONTB_BRAIN_JOB_INTERVAL = 3000;
+    public static final String SERVER_IDENTITY_TOKEN = "aaabbbccc";
 }
