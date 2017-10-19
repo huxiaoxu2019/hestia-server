83b7560968b976e2ba59ed7e19c6bab0e4e0471c
genialx <admin@ihuxu.com>
2017-10-19 13:37:39 +0800

    change build script

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 echo "compiling..."
 cd src
-javac -d ../bin com/ihuxu/hestia/server/controller/Bootstrap.java
+javac -d ../bin com/ihuxu/hestia/server/controller/BootstrapController.java -Xlint:deprecation
 
 echo "building..."
 cd ../bin
 mkdir ../run
 chmod -R 777 ../run
-nohup java com.ihuxu.hestia.server.controller.Bootstrap > ../run/server.log &   
+nohup java com.ihuxu.hestia.server.controller.BootstrapController > ../run/server.log &   
